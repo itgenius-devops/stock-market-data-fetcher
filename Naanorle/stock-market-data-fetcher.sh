8e38468bffa994117f73be202e437d6a361d9bc9
Naanorle <chris.n.odonkor@gmail.com>
2025-04-12 05:52:21 +0000

    Modifications made

--- Naanorle/stock-market-data-fetcher.sh
+++ Naanorle/stock-market-data-fetcher.sh
@@ -2,28 +2,31 @@
 
 # Author: Naanorle
 
-# Description: A Shell script that downloads stock data in both JSON and CSV format on different days 
+# Description: A Shell script that downloads stock data in both JSON and CSV format on different dates
 
-# Defining the variable Date: Different days for the downloads respectively
+# Defining Dates as a variable
 
 Dates=("2025-04-09" "2025-04-10" "2025-04-11")
 
-# Processing the different dates for the download. This fore loop will run sequentially to download the files for the  different dates
+#This fore loop will run sequentially to download the files for the  different dates
 
 for Date in "${Dates[@]}"; do
+{
+   echo "Processing $Date"
 
-    echo "Processing $Date"
+# Downloads stock data in csv
 
-# Download stock data in Csv format for each date
-
-wget "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&outputsize=full&datatype=json&apikey=L2CS5Y3MI25MWQRY" -O /home/ec2-user/git_assignment/stock-market-data-fetcher/Naanorle/csv/ibm-intraday-$Date.csv
-
-
-# Download stock data in Json format for each date
-
-wget "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&outputsize=full&datatype=json&apikey=L2CS5Y3MI25MWQRY" -O /home/ec2-user/git_assignment/stock-market-data-fetcher/Naanorle/json/ibm-intraday-$Date.json
+wget "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&outputsize=full&datatype=json&apikey=L2CS5Y3MI25MWQRY" -O /home/ec2-user/git_assignment/stock-market-data-
+fetcher/Naanorle/csv/ibm-intraday-$Date.csv"
 
 
+# Downloads stock data in json
+
+wget "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&outputsize=full&datatype=json&apikey=L2CS5Y3MI25MWQRY" -O /home/ec2-user/git_assignment/stock-market-data-fetcher/Naanorle/json/ibm-intraday-$Date.json"
+
+} > /home/ec2-user/git_assignment/stock-market-data-fetcher/Naanorle/logs/ibm-intraday-$Date.log 2>&1
 done
 
-echo "Both downloads completed, thank you. Byee!"
+echo "All downloads completed"
+
+
